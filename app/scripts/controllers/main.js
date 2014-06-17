19fc2d4567768c36e80fe95b5c27e54674dbde3c
Suresh Balasubramian <suresh@ctidigital.com>
2014-06-17 13:27:40 +0100

    First commit with yeoman boilerplate

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('musictiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
